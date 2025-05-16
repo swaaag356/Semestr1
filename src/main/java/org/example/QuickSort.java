b0d1289af9003391813531243fa30fbb2f90aa04
Ilnaz Galiev <qazxswedc20063006@gmail.com>
2025-05-16 13:19:31 +0300

    QuickSort

--- src/main/java/org/example/QuickSort.java
+++ src/main/java/org/example/QuickSort.java
@@ -1,17 +1,58 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class QuickSort {
+
+    static long iteration = 0;
+
+    public static void main(String[] args) throws IOException {
+        int[] arr = {123,4,132,4,1324,324,213,3213,4,33,54345,6352,3423345,34234,2334};
+        quickSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+            iteration++;
         }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int middle = low + (high - low) / 2;
+        int pivot = arr[middle];
+        int temp = arr[middle];
+        arr[middle] = arr[high];
+        arr[high] = temp;
+        iteration++;
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                iteration++;
+            }
+        }
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        iteration++;
+        return i + 1;
+    }
+
+    public static int[] readArrayFromFile(File file) throws IOException {
+        String content = file.toString();
+        return Arrays.stream(content.trim().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
